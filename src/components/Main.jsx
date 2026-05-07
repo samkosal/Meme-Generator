ee484e223fa605a3e8097371921c8e21b46c55a8
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-07 14:45:07 -0700

    useEffects learned, added useEffect for meme API

---
+++ src/components/Main.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+export default function Main() {
+
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageURL: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemes, setAllMemes] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.imgflip.com/get_memes`)
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, []);
+
+
+
+    function handleChange(event) {
+        const { value, name } = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+        console.log(value)
+    }
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageURL} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
